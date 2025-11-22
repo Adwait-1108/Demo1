80c6d23831f4b954baaf539fb31796f20d2e4571
Adwait <adwaitvid@gmail.com>
2025-11-22 12:46:48 +0530

    Factorial function added

--- app.py
+++ app.py
@@ -3,4 +3,10 @@ def odd_even(n):
         print("Even")
     else:
         print("Odd")
-        
+
+
+def factorial(n):
+    if n==0 or n==1:
+        return 1
+    else:
+        return n * factorial(n-1)
